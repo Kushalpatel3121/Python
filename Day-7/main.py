062663eddd2e8ef33de41035455830fb44769381
Kushal Patel <kushal.patel3121@gmail.com>
2022-07-05 22:16:44 +0530

    Day-7 Completed

--- Day-7/main.py
+++ Day-7/main.py
@@ -3,13 +3,23 @@
 import random
 word_list = ['cricket','football','beekeeper']
 chosen = random.choice(word_list)
-
-guess = input("Enter your choice : ")
-guess.lower()
-
-for i in range(0,len(chosen)):
-    if guess == chosen[i]:
-        print("Right")
-    else:
-        print("Wrong")
-
+display = []
+for letter in chosen:
+    display += '_'
+lives = 6
+end_of_game = False
+while not end_of_game:
+    guess = input("Enter your choice : ").lower()
+    for i in range(0,len(chosen)):
+        letter = chosen[i]
+        if guess == letter:
+            display[i] = letter
+    if guess not in chosen:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose!")
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!")
